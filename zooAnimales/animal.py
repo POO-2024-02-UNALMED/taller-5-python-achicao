8bac77aefc8cda29cfec7261a836471f80b769a5
achicao <achicao@unal.edu.co>
2024-12-13 18:50:28 -0500

    Intento 2

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -1,9 +1,3 @@
-from zooAnimales.anfibio import Anfibio
-from zooAnimales.ave import Ave
-from zooAnimales.mamifero import Mamifero
-from zooAnimales.pez import Pez
-from zooAnimales.reptil import Reptil
-
 class Animal():
     _totalAnimales = 0
 
@@ -13,17 +7,24 @@ class Animal():
         self._habitat = habitat
         self._genero = genero
         self._zona = zona
-        _totalAnimales += 1
+        Animal._totalAnimales += 1
     
     def movimiento(self):
         return "desplazarse"
     
+    @staticmethod
     def totalPorTipo(self):
+        from zooAnimales.mamifero import Mamifero
+        from zooAnimales.ave import Ave
+        from zooAnimales.reptil import Reptil
+        from zooAnimales.pez import Pez
+        from zooAnimales.anfibio import Anfibio
+
         return f"Mamiferos: {Mamifero.cantidadMamiferos}\nAves: {Ave.cantidadAves}\nReptiles: {Reptil.cantidadReptiles}\nPeces: {Pez.cantidadPeces}\nAnfibios: {Anfibio.cantidadAnfibios}"
 
     def __str__(self):
         
-        if len(self._zona) != 0:
+        if self._zona != None:
             return f"Mi nombre es {self.nombre}, tengo una edad de {self.edad}, habito en {self.habitat} y mi genero es {self.genero}, la zona en la que me ubico es {self.zona[0]}, en el {self.zona[0].getZoo().getNombre()}"
         else:
             return f"Mi nombre es {self.nombre}, tengo una edad de {self.edad}, habito en {self.habitat} y mi genero es {self.genero}"   
